05477b9aad0c6b62c76239df8c69d09ad4977548
naodmulu <naodmulugeta1621@gmail.com>
2023-02-09 10:01:49 +0300

    Evaluate Reverse Polish Notation

---
+++ Week 4/Evaluate Reverse Polish Notation.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+        for token in tokens:
+            if token == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif token == "-":
+                stack.append(-stack.pop() + stack.pop())
+            elif token == "*":
+                stack.append(stack.pop() * stack.pop())
+            elif token == "/":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(int(float(b) / a))
+            else:
+                stack.append(int(token))
+        return stack.pop()
